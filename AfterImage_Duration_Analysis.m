298ed2537f9fad9a188980743700cb421e45cc89
Ian <ian@aristotle>
2016-12-12 15:45:26 +0800

    add duration anal

---
+++ AfterImage_Duration_Analysis.m
@@ -0,0 +1,73 @@
+errortype = 'sem';
+
+revision = 3;
+
+if revision == 3
+	in = load('Duration Data Revision 3.mat');
+	in = in.Vdata;
+	aidat=in(:,3);
+	contrast=in(:,2);
+	sdur=in(:,1);
+	contrast(contrast==0)=-0.5;
+	contrast(contrast==0.2)=-0.3;
+	contrast(contrast==0.4)=-0.1;
+	contrast(contrast==0.6)=0.1;
+	contrast(contrast==0.8)=0.3;
+	contrast(contrast==1)=0.5;
+else
+	in = load('Durationdata.mat');
+	in = in.Durationdata;
+	aidat=in(:,1);
+	contrast=in(:,2);
+	sdur=in(:,3);
+	contrast(contrast==0)=-0.5;
+	contrast(contrast==115)=-0.3;
+	contrast(contrast==159)=-0.1;
+	contrast(contrast==191)=0.1;
+	contrast(contrast==218)=0.3;
+	contrast(contrast==255)=0.5;
+end
+
+aidur=table(aidat, contrast, sdur,'VariableNames',{'AI_Duration','Contrast','Stimulus_Duration'});
+
+clear g;
+figure('Position',[10 50 1000 1000],'Name','Duration Results');
+g(1,1)=gramm('x',aidur.Contrast,'y',aidur.AI_Duration,'color',aidur.Stimulus_Duration);
+g(1,1).set_color_options('map','matlab');
+g(1,1).set_names('y','Afterimage Duration(s)','color','Stimulus Duration(s)','x','Contrast');
+g(1,1).set_order_options('x',0);
+g(2,1)=copy(g(1));
+g(1,2)=copy(g(1));
+g(2,2)=copy(g(1));
+%g(1,1).facet_grid([],pdata.Contrast,'scale','fixed');
+g(1,1).geom_jitter('width',0.3,'height',0);
+g(1,1).stat_glm('disp_fit','true');
+g(2,1).stat_summary('type',errortype,'geom','area','setylim','true'); g(2,1).no_legend;
+g(2,1).set_title(errortype);
+g(1,2).stat_boxplot('width',0.7,'dodge',0.5); g(1,2).no_legend;
+g(2,2).stat_smooth(); g(2,2).no_legend;
+g.axe_property('XGrid','on','YGrid','on','Box','on');
+g.set_title('Relation of Afterimage Duration to Contrast and Stimulus duration');
+g.draw;
+
+
+[p2,tbl3,stats2]=anovan(aidur.AI_Duration,{aidur.Stimulus_Duration,aidur.Contrast},'model','interaction','varnames',{'StimDuration','Contrast'});set(gcf,'Name','Duration Results')
+figure('Position',[20 60 1000 1000],'Name','Duration Results')
+[c,~,~,gnames]=multcompare(stats2,'CType','tukey-kramer','Dimension',[1 2]);set(gcf,'Name','Duration Results')
+ptable=[gnames(c(:,1)),gnames(c(:,2)),num2cell(c(:,3:6))];
+
+eightsec.data = aidur.AI_Duration(aidur.Stimulus_Duration==8);
+eightsec.contrast = aidur.Contrast(aidur.Stimulus_Duration==8);
+
+[p,tbl4,stats]=anova1(eightsec.data,eightsec.contrast,'varnames',{'Ctrst'});set(gcf,'Name','Duration Results for 8 Secs Duration')
+figure('Position',[10 40 1000 1000],'Name','Duration Results for 8 Secs Duration')
+[c,~,~,gnames]=multcompare(stats,'CType','tukey-kramer');set(gcf,'Name','Duration Results for 8 Secs Duration')
+ptable2=[gnames(c(:,1)),gnames(c(:,2)),num2cell(c(:,3:6))];
+
+foursec.data = aidur.AI_Duration(aidur.Stimulus_Duration==4);
+foursec.contrast = aidur.Contrast(aidur.Stimulus_Duration==4);
+
+[p,tbl5,stats]=anova1(foursec.data,foursec.contrast,'varnames',{'Ctrst'});set(gcf,'Name','Duration Results for 4 Secs Duration')
+figure('Position',[10 40 1000 1000],'Name','Duration Results for 4 Secs Duration')
+[c,~,~,gnames]=multcompare(stats,'CType','tukey-kramer');set(gcf,'Name','Duration Results for 4 Secs Duration')
+ptable3=[gnames(c(:,1)),gnames(c(:,2)),num2cell(c(:,3:6))];
--- loadEyeData.m
+++ loadEyeData.m
@@ -10,44 +10,71 @@ e = eyelinkAnalysis('file',fn,'dir',pn);
 
 switch analyze
 	case 'contrast'
-	e.variableMessageName = 'PEDESTAL';
-	e.pixelsPerCm					= s.pixelsPerCm;
-	e.distance						= s.distance;
-	e.xCenter							= s.xCenter;
-	e.yCenter							= s.yCenter;
-	e.correctValue				= [1 2 3]; %NOSEE YESBRIGHT YESDARK
-	e.incorrectValue			= [0 4]; %UNSURE
-	e.breakFixValue				= -1;
-	e.measureRange				= [-0.5 5];
-	e.plotRange						= [-0.5 4];
-	e.excludeIncorrect		= false;
-	e.simpleParse;
-	e.pruneNonRTTrials;
-	e.parseSaccades;
-	
-	responses = task.response.response;
-	contrasts = task.response.contrastOut;
-	pedestals = task.response.pedestal;
-	pedEye = [e.trials(e.correct.idx).variable];
-	responsesEye = [e.trials(e.correct.idx).variable];
-	
-	blackIdx = e.correct.idx(contrasts==0);
-	whiteIdx = e.correct.idx(contrasts==1);
-	
+		e.variableMessageName = 'PEDESTAL';
+		e.pixelsPerCm					= s.pixelsPerCm;
+		e.distance						= s.distance;
+		e.xCenter							= s.xCenter;
+		e.yCenter							= s.yCenter;
+		e.correctValue				= [1 2 3]; %NOSEE YESBRIGHT YESDARK
+		e.incorrectValue			= [0 4]; %UNSURE
+		e.breakFixValue				= -1;
+		e.measureRange				= [-0.5 5];
+		e.plotRange						= [-0.5 4];
+		e.excludeIncorrect		= false;
+		e.simpleParse;
+		e.pruneNonRTTrials;
+		%e.parseSaccades;
+		
+		responses = task.response.response;
+		contrasts = task.response.contrastOut;
+		pedestals = task.response.pedestal;
+		responsesEye = [e.trials(e.correct.idx).result];
+		pedEye = [e.trials(e.correct.idx).variable];
+		
+		% we may have some rogue trials we need to exclude, casued by not always
+		% clearing response on a new trial, thus sometimes a failed trial (breakfix etc)
+		% was still given a non-error response value. Only way to correct is parse the
+		% response vector from task.response and identify eyelink trials where response
+		% is repeated. This code iterates through and finds the repeat trials.
+		iEye = 1;
+		erroridx = [];
+		cidx = [];
+		for i = 1:length(responses)
+			if responses(i) == responsesEye(iEye)
+				cidx(i) = e.correct.idx(iEye);
+				iEye = iEye+1;
+				continue
+			else
+				erroridx(end+1) = iEye;
+				iEye = iEye + 1;
+				cidx(i) = e.correct.idx(iEye);
+				iEye = iEye + 1;
+				continue
+			end
+		end
+		
+		responsesEye = [e.trials(cidx).result];
+		pedEye = [e.trials(cidx).variable];
+		
+		if length(contrasts) == length(cidx)
+			blackIdx = cidx(contrasts==0);
+			whiteIdx = cidx(contrasts==1);
+		end
+		
 	case 'latency'
-	e.variableMessageName		= 'TRIALID';
-	e.pixelsPerCm						= s.pixelsPerCm;
-	e.distance							= s.distance;
-	e.xCenter								= s.xCenter;
-	e.yCenter								= s.yCenter;
-	e.correctValue					= [0 1]; %NOSEE YESBRIGHT YESDARK
-	e.incorrectValue				= [4]; %UNSURE
-	e.breakFixValue					= -1;
-	e.measureRange					= [-0.5 5];
-	e.plotRange							= [-0.5 4];
-	e.excludeIncorrect			= false;
-	e.simpleParse;
-	e.pruneNonRTTrials;
-	e.parseSaccades;
-	e.plot(e.correct.idx(1:10));
+		e.variableMessageName		= 'TRIALID';
+		e.pixelsPerCm						= s.pixelsPerCm;
+		e.distance							= s.distance;
+		e.xCenter								= s.xCenter;
+		e.yCenter								= s.yCenter;
+		e.correctValue					= [0 1]; %NOSEE YESBRIGHT YESDARK
+		e.incorrectValue				= [4]; %UNSURE
+		e.breakFixValue					= -1;
+		e.measureRange					= [-0.5 5];
+		e.plotRange							= [-0.5 4];
+		e.excludeIncorrect			= false;
+		e.simpleParse;
+		e.pruneNonRTTrials;
+		e.parseSaccades;
+		e.plot(e.correct.idx(1:10));
 end
